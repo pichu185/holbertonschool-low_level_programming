05a18457b4e39683c464f5482a76e8b3e271b76c
Pichu185 <pichu_otegui@hotmail.com>
2021-02-04 20:01:13 +0000

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '7'; a++)
+		for (b = a + 1; b <= '8'; b++)
+			for (c = b + 1; c <= 9; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (c == '7' && b == '8' && c == '9')
+				{
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	putchar('\n');
+	return (0);
+}
