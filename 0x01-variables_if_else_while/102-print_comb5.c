32681e67a3cf65294c1f2ef06d9653d79b709e23
Pichu185 <pichu_otegui@hotmail.com>
2021-02-05 01:01:14 +0000

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+}
