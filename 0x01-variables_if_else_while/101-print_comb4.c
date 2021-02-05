3aab7a404f5d5a13a9e08e8b3151263973eff709
Pichu185 <pichu_otegui@hotmail.com>
2021-02-05 00:55:36 +0000

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ int main(void)
 
 	for (a = '0'; a <= '7'; a++)
 		for (b = a + 1; b <= '8'; b++)
-			for (c = b + 1; c <= 9; c++)
+			for (c = b + 1; c <= '9'; c++)
 			{
 				putchar(a);
 				putchar(b);
