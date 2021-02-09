982f0ccd81f817fadfb6477530e8e766c420e7c4
Pichu185 <pichu_otegui@hotmail.com>
2021-02-09 21:18:11 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _abs - Entry point
+ * @x: wopa
+ *
+ * Description: Show a message blablabla
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return (-x);
+
+}
