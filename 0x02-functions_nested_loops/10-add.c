a5748dae5c0b4025360e892ca8559a7642fb5840
Pichu185 <pichu_otegui@hotmail.com>
2021-02-09 21:46:01 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - Entry point
+ *@x: wopa
+ *@y: wopa 2
+ *
+ * Description: Show a message blablabla
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
