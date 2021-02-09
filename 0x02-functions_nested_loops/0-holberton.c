5e2cccf1fdaccec10f0ef2cc874346ab75c61f15
Pichu185 <pichu_otegui@hotmail.com>
2021-02-09 16:33:49 +0000

    0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Show a message blablabla
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+putchar('H');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
+putchar('\n');
+  return (0);
+}
