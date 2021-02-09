7da4f376412ca8d9fdcc75f88c05cb0c17598459
Pichu185 <pichu_otegui@hotmail.com>
2021-02-09 21:01:20 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_sign - Entry point
+ * @n: wopa
+ *
+ * Description: Show a message blablabla
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+		if (n < 0)
+		{
+			_putchar('-');
+			return (-1);
+		}
+		else
+		{
+			_putchar('0');
+			return (0);
+		}
+}
