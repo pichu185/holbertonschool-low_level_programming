f78967c9aa1f494bb86b040d68fa7150cd58fcc7
Pichu185 <pichu_otegui@hotmail.com>
2021-02-11 17:02:06 +0000

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * mul- multiplication
+ * @a: integer
+ * @b: integer
+ * Return: return multiplication between a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
