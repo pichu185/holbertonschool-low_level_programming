bc99e1087ad40459e6c78e192988a0f227799732
Pichu185 <pichu_otegui@hotmail.com>
2021-02-11 18:03:31 +0000

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-include "holberton.h"
+#include "holberton.h"
 /**
  * print_numbers - print numbers from 0 to 9
  *
@@ -8,8 +8,6 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-	{
-		_putchar(i + '0');
-	}
+       	_putchar('0' + i);
 	_putchar('\n');
 }
