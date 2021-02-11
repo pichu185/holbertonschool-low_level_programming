06930a2259cd14331bd6270074e4a7b3faea3a94
Pichu185 <pichu_otegui@hotmail.com>
2021-02-11 16:45:14 +0000

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _isdigit - Entry point
+ * @c: wopa
+ *
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 0) && (c < 9))
+		return (1);
+	else
+		return (0);
+}
