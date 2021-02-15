7c514b48457eab59b918fdfaf044805e02f3274e
Pichu185 <pichu_otegui@hotmail.com>
2021-02-15 22:58:45 +0000

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * reset_to_98 - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 
 void reset_to_98(int *n);
-
-
-int n;
+{
+	*n = 98;
+}
