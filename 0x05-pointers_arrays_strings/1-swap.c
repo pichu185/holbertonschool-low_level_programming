8c55305dbda45173019402ef6e015a291cebec8f
Pichu185 <pichu_otegui@hotmail.com>
2021-02-16 01:15:17 +0000

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * swap_int - check the code for Holberton School students.
+ * @a: wopa
+ * @b: wooopa
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
