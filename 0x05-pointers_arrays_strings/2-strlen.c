e07708d383f018c2c614a8de9ccc7e86def2925c
Pichu185 <pichu_otegui@hotmail.com>
2021-02-16 10:11:51 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: wopa
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	}
+	return (a);
+}
