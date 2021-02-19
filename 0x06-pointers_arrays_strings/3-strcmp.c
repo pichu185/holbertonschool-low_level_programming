36fbe3543ac02483d781d824166827e207d46c90
Pichu185 <pichu_otegui@hotmail.com>
2021-02-19 22:22:55 +0000

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,9 +9,9 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int count = 0;
+	int i = 0;
 
-	while ((s1[count] && s2[count]) && (s1[count] == s2[count]))
-		count++;
-	return (s1[count] - s2[count]);
+	while ((s1[i] != '\0' && s2[i] != '\0') && (s1[i] == s2[i]))
+		i++;
+	return (s1[i] - s2[i]);
 }
