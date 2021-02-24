3974dd4b34275115b485e2d5ea0c358f6e2a2e3b
Pichu185 <pichu_otegui@hotmail.com>
2021-02-24 01:35:54 +0000

    9-set_string.c

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - check the code for Holberton School students.
+ * @s: abc
+ * @to: def
+ * Return: Always 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
