0740fa437138657764671355e9f24a631033c39a
pichu185 <pichu_otegui@hotmail.com>
2021-03-08 16:48:59 +0000

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - file name
+ * Description: Write a program that prints the name of the
+ * file it was compiled from, followed by a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
