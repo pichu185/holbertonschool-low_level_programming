3f31da1a87a78b40c80eb8fff4b34de279282fcb
pichu185 <pichu_otegui@hotmail.com>
2021-03-10 22:26:21 +0000

    performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -6,8 +6,8 @@
  * function to perform the operation asked by the user.
  * @s: is the operator passed as argument to the program
  * Return: operation asked by user
- * Description: This function returns a pointer to the function that corresponds
- * to the operator given as a parameter.
+ * Description: This function returns a pointer to the function that
+ * corresponds to the operator given as a parameter.
  */
 
 int (*get_op_func(char *s))(int, int)
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,36 +3,59 @@
 
 /**
  * op_add - returns the sum of a and b
+ * @a: First number
+ * @b: Second number
+ * Return: calculation
+ * Description: Write a program that performs simple operations
+ */
+int op_add(int a, int b)
+{
+		return (a + b);
+}
+
+/**
  * op_sub - returns the difference of a and b
+ * @a: First number
+ * @b: Second number
+ * Return: calculation
+ * Description: Write a program that performs simple operations
+ */
+int op_sub(int a, int b)
+{
+		return (a - b);
+}
+
+/**
  * op_mul - returns the product of a and b
+ * @a: First number
+ * @b: Second number
+ * Return: calculation
+ * Description: Write a program that performs simple operations
+ */
+int op_mul(int a, int b)
+{
+		return (a * b);
+}
+
+/**
  * op_div - returns the result of the division of a by b
+ * @a: First number
+ * @b: Second number
+ * Return: calculation
+ * Description: Write a program that performs simple operations
+ */
+int op_div(int a, int b)
+{
+		return (a / b);
+}
+
+/**
  * op_mod - returns the remainder of the division of a by b
  * @a: First number
  * @b: Second number
  * Return: calculation
  * Description: Write a program that performs simple operations
  */
-
-int op_add(int a, int b)
-{
-		return (a + b);
-}
-
-int op_sub(int a, int b)
-{
-		return (a - b);
-}
-
-int op_mul(int a, int b)
-{
-		return (a * b);
-}
-
-int op_div(int a, int b)
-{
-		return (a / b);
-}
-
 int op_mod(int a, int b)
 {
 		return (a % b);
