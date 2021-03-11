1879a42a86ab5eebac0627199daae1f59d0b1084
pichu185 <pichu_otegui@hotmail.com>
2021-03-11 23:28:12 +0000

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,27 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * struct datatype - datatype structure
+ * @type: type of structure
+ * @func: function
+ * Return: Always 0
+ */
+
+typedef struct datatype
+{
+	char *type;
+	void (*func)(va_list list);
+} data_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif
