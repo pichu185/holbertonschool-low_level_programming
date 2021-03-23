acea5ea3e10b3f39aa61e8c8e40823b595285b7e
pichu185 <pichu_otegui@hotmail.com>
2021-03-23 14:42:03 +0000

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements of a listint_t list
+ * @h: pointer to the list
+ * Return: number of nodes
+ **/
+size_t listint_len(const listint_t *h)
+{
+	unsigned int contador = 0;
+	const listint_t *nodo = h;
+
+	while (nodo->next != NULL)
+	{
+		nodo = nodo->next;
+		contador = contador + 1;
+	}
+	return (contador);
+}
