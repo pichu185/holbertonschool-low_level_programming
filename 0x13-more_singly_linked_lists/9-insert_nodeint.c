a084d83129529d9201a884a8e1a24bb274846f97
pichu185 <pichu_otegui@hotmail.com>
2021-03-24 02:09:10 +0000

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	/* general case */
 	if (contador == index && nodo)
 	{
-		nodo->next = nodo->next;
+		new->next = nodo->next;
 		nodo->next = new;
 		return (new);
 	}
