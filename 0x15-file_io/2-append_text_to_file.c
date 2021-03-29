c68ac9f6d5a334d3aff4ca55547d17dcc50eff45
pichu185 <pichu_otegui@hotmail.com>
2021-03-29 22:44:24 +0000

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file to append the text
+ * @text_content: the NULL terminated string to add at the end of the file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, largo = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd < 0)
+		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	while (text_content[largo])
+		largo++;
+	if (write(fd, text_content, largo) < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
