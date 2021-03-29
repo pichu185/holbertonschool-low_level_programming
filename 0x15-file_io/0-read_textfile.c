080d817870fcad831a777e062c90da09df61e466
pichu185 <pichu_otegui@hotmail.com>
2021-03-29 20:21:17 +0000

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,24 +10,32 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd = 0;
-	ssize_t imp = 0;
+	ssize_t reading = 0;
+	ssize_t writing = 0;
 	char *buff;
 
 	if (!filename)
 		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
-		return (0);
-	imp = read(fd, buff, letters);
-	if (imp != -1)
 	{
-		if (write(STDOUT_FILENO, buff, letters) == -1)
+		free(buff);
+		return (0);
+	}
+	reading = read(fd, buff, letters);
+	if (reading != -1)
+	{
+		writing = write(STDOUT_FILENO, buff, reading);
+		if (writing == -1)
+		{
+			free(buff);
 			return (0);
+		}
 	}
 	free(buff);
 	close(fd);
-	return (imp);
+	return (reading);
 }
