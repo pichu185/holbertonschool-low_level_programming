60509d2ac59723a6e46ccec083ea229ea1b6883b
pichu185 <pichu_otegui@hotmail.com>
2021-05-02 15:37:31 +0000

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
  * @h: pointer to the list
- * return: the number of nodes
+ * Return: the number of nodes
  **/
 size_t print_dlistint(const dlistint_t *h)
 {
