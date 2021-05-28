1606e497ea30659f309bb557a86f7bbd5d6ef400
Pichu185 <pichu_otegui@hotmail.comm>
2021-05-28 16:10:22 -0300

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,21 +8,21 @@
  **/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *new_table = NULL;
-        unsigned long int i = 0;
+    hash_table_t *new_table = NULL;
+    unsigned long int i = 0;
 
-        new_table = malloc(sizeof(hash_node_t));
-        if (!new_table) 
-                return (NULL);
+    new_table = malloc(sizeof(hash_node_t));
+    if (!new_table) 
+        return (NULL);
 
-        new_table->size = size;
-        new_table->array = malloc(sizeof(hash_node_t**) * size);
-        if (!new_table->array)
+    new_table->size = size;
+    new_table->array = malloc(sizeof(hash_node_t **) * size);
+    if (!new_table->array)
         {
-                free(new_table);
-                return (NULL); 
+            free(new_table);
+            return (NULL); 
         }
-        for (i = 0; i < size; i++)
-                (new_table->array[i] = 0);
-        return (new_table);
+    for (i = 0; i < size; i++)
+        (new_table->array[i] = 0);
+    return (new_table);
 }
