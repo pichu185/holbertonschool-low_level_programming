b10332092d292d241d620052020aadcaebce6a2b
pichu185 <pichu_otegui@hotmail.com>
2021-05-28 22:47:43 +0000

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,39 +9,39 @@
  **/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *element_node, *aux;
-    unsigned long int index;
+	hash_node_t *element_node, *aux;
+	unsigned long int index;
 
-    if (!ht || !key || ! value || *key == "\0")
-        return (0);
+	if (!ht || !key || !value || *key == '\0')
+		return (0);
 
-    /* getting the index of the key with previous function*/
-    index = key_index((const unsigned char *)key, ht->size);
+	/* getting the index of the key with previous function*/
+	index = key_index((const unsigned char *)key, ht->size);
 
-    /* creating pointer to head of the array */
-    aux = ht->array[index];
+	/* creating pointer to head of the array */
+	aux = ht->array[index];
 
-    while (aux)
-    {
-        /*checking if the key matches the key in the node*/
-        if (strcmp(key, aux->key) == 0)
-        {
-            free(aux->value);
-            aux->value = strdup(value);
-            return(1);
-        }
-        else
-            aux = aux->next;
-    }
-    /* if key was not found, we create a new node */
-    element_node = malloc(sizeof(hash_node_t));
-    if (!element_node)
-        return(0);
-    
-    element_node->key = strdup(key);
-    element_node->value = strdup(value);
-    
-    element_node->next = ht->array[index];
-    ht->array[index] = element_node;
-    return(1);
-}   
+	while (aux)
+	{
+		/*checking if the key matches the key in the node*/
+		if (strcmp(key, aux->key) == 0)
+		{
+			free(aux->value);
+			aux->value = strdup(value);
+			return (1);
+		}
+		else
+			aux = aux->next;
+	}
+	/* if key was not found, we create a new node */
+	element_node = malloc(sizeof(hash_node_t));
+	if (!element_node)
+		return (0);
+
+	element_node->key = strdup(key);
+	element_node->value = strdup(value);
+
+	element_node->next = ht->array[index];
+	ht->array[index] = element_node;
+	return (1);
+}
