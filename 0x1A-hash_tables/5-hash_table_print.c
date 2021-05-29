ba4b5f5b1e6fcc0644adc39ec8070c4b275ba6c2
Pichu185 <pichu_otegui@hotmail.comm>
2021-05-28 21:27:01 -0300

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: pointer to the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+hash_node_t *temporal_node;
+unsigned long int i;
+int coma = 0;
+
+    if (!ht)
+        return;
+
+    print("{");
+
+    for (i = 0; i < ht->size; i++)
+    {
+        if (ht->array[i] != NULL)
+        {
+            /* in each index of the array we set the pointer to the head */
+            temporal_node = ht->array[i];
+            while (temporal_node)
+            {
+                if (!temporal_node->key)
+                    continue;
+                /* printing ', ' on each value after the first one */
+                if (coma == 1)
+                    print(", ");
+                
+                /* print the key followed by the value */
+                print("'%s': '%s'", temporal_node->key, temporal_node->value);
+                
+                temporal_node = temporal_node->next;           
+            }
+            coma = 1;
+        }
+    }
+    print("}\n");
+}
