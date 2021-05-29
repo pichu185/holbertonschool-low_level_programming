af4305ac03f9c461f8b3a484fcadb51c44c10c76
pichu185 <pichu_otegui@hotmail.com>
2021-05-29 00:31:09 +0000

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,36 +6,36 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-hash_node_t *temporal_node;
-unsigned long int i;
-int coma = 0;
+	hash_node_t *temporal_node;
+	unsigned long int i;
+	int coma = 0;
 
-    if (!ht)
-        return;
+	if (!ht)
+		return;
 
-    print("{");
+	printf("{");
 
-    for (i = 0; i < ht->size; i++)
-    {
-        if (ht->array[i] != NULL)
-        {
-            /* in each index of the array we set the pointer to the head */
-            temporal_node = ht->array[i];
-            while (temporal_node)
-            {
-                if (!temporal_node->key)
-                    continue;
-                /* printing ', ' on each value after the first one */
-                if (coma == 1)
-                    print(", ");
-                
-                /* print the key followed by the value */
-                print("'%s': '%s'", temporal_node->key, temporal_node->value);
-                
-                temporal_node = temporal_node->next;           
-            }
-            coma = 1;
-        }
-    }
-    print("}\n");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+		/* in each index of the array we set the pointer to the head */
+			temporal_node = ht->array[i];
+			while (temporal_node)
+			{
+				if (!temporal_node->key)
+					continue;
+				/* printing ', ' on each value after the first one */
+				if (coma == 1)
+					printf(", ");
+
+				/* print the key followed by the value */
+				printf("'%s': '%s'", temporal_node->key, temporal_node->value);
+
+				temporal_node = temporal_node->next;
+			}
+			coma = 1;
+		}
+	}
+	printf("}\n");
 }
