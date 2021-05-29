300e2774bc65e07ac0e992bf1d004109a2cde1b1
pichu185 <pichu_otegui@hotmail.com>
2021-05-29 00:59:40 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
-	unsigned long int i;
+	unsigned int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
