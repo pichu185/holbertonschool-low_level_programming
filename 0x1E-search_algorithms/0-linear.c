329906e6ad868a7100fa01aa3feb875d5e29cad2
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-09-01 17:25:56 -0300

    0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function that searches for a value in an array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size:  is the number of elements in array
+ * @value: is the value to search for
+ * Return: the first index where value is located or -1 if Null or not in array
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+        size_t j;
+
+	if (!array)
+		return (-1);
+
+	for (j = 0; j < size; j++)
+        {
+                printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+                if (array[j] == value)
+			return (j);
+        }
+        return (-1);
+}
